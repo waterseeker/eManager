e2c1b3dcb4223996b075037a3a346479967ffc06
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-26 09:34:45 -0500

    added CreateEmployeeViewModel with some attributes [HiddenInput], [Required], [DataType]

---
+++ eManager.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eManager.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ eManager.Web/Models/CreateEmployeeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eManager.Web.Models
+{
+    public class CreateEmployeeViewModel
+    {
+        [HiddenInput(DisplayValue =false)] //sets value to hidden and not displayed at all
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)] //treat the datetime as just a date since we don't need to know anything but the date
+        //for a hire date. 
+        public DateTime HireDate { get; set; }
+
+    }
+}
