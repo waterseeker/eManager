c9228d2b4593a7249207e65bde3affee55b992bf
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-26 09:12:54 -0500

    added detail view for each department

---
+++ eManager.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using eManager.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eManager.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentDataSource _db;
+        public DepartmentController(IDepartmentDataSource db)
+        {
+            _db = db;
+        }
+        public ActionResult Detail(int id)
+        {
+            var model = _db.Departments.Single(d => d.Id == id);
+            return View(model);
+        }
+    }
+}
